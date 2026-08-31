4fa33631235f55616c96b6c7a78b3764ec865139
Brayden Wayman <bwayman@byupathway.edu>
2026-08-31 11:45:29 -0600

    learning activity

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,26 +3,19 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
+
         var intResult = ListSelector(l1, l2, select);
-        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}");
 
-        var l3 = new[] { 'A', 'A', 'A', 'A', 'A'};
-        var l4 = new[] { 'B', 'B', 'B', 'B', 'B'};
-        select = new[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
+        var l3 = new[] { 'A', 'A', 'A', 'A', 'A' };
+        var l4 = new[] { 'B', 'B', 'B', 'B', 'B' };
+
+        select = new[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+
         var charResult = ListSelector(l3, l4, select);
-        Console.WriteLine("<char[]>{" + string.Join(", ", charResult) + "}"); // <char[]>{A, B, A, B, A, B, A, B, A, B}
-    }
-
-    private static T[] ListSelector<T>(T[] list1, T[] list2, int[] select)
-    {
-        var result = new T[select.Length];
-        for (int i = 0; i < select.Length; i++)
-        {
-            result[i] = select[i] == 1 ? list1[i] : list2[i];
-        }
-        return result;
+        Console.WriteLine("<char[]>{" + string.Join(", ", charResult) + "}");
     }
 
     private static T[] ListSelector<T>(T[] list1, T[] list2, int[] select)
@@ -30,6 +23,7 @@ public static class ArraySelector
         var result = new T[select.Length];
         var l1Idx = 0;
         var l2Idx = 0;
+
         for (var i = 0; i < select.Length; i++)
         {
             if (select[i] == 1)
