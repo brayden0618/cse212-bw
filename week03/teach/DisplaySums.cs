6f82cb74b2c37344443caab2390bf54edf578daf
Brayden Wayman <bwayman@byupathway.edu>
2026-09-16 16:49:46 -0600

    team_activity

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -28,6 +28,17 @@
     /// </summary>
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
+        // TODO Problem 2 - Implement this method to display pairs of numbers that sum to 10
+        var seenNumbers = new HashSet<int>();
+
+        foreach (var number in numbers) {
+            int complement = 10 - number;
+
+            if (seenNumbers.Contains(complement)) {
+                Console.WriteLine($"{number} {complement}");
+            }
+
+            seenNumbers.Add(number);
+        }
     }
 }
